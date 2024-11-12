c349b7436283841549bf8702faade6c736f5c4aa
Seakuh <eteranimal@protonmail.com>
2024-11-12 17:24:29 +0100

    add footer

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+// src/components/Footer.tsx
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footer-container">
+        FOOTER
+    </div>
+  );
+};
+
+export default Footer;
