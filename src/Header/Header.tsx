c88ee80039b7ab6166831c4718d28a2ce6a308e5
Seakuh <eteranimal@protonmail.com>
2024-11-12 17:24:37 +0100

    add header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,13 @@
+// src/components/Header.tsx
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <div className="Header-container">
+        HEADER
+    </div>
+  );
+};
+
+export default Header;
