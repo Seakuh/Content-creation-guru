02bf34a46b1e0bd0b2049a38c2cd7d47de25c588
Seakuh <eteranimal@protonmail.com>
2024-11-12 21:08:53 +0100

    add login for later

---
+++ src/Login/Login.tsx
@@ -0,0 +1,13 @@
+// src/components/Login/Login.tsx
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Login-Seite</h2>
+      <p>Hier können Sie sich anmelden.</p>
+    </div>
+  );
+};
+
+export default Login;
